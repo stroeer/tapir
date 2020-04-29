70abb91ebd9016a15f5e936bf6b0f69e67b97603
Harry Urban <hurban@stroeer.de>
2020-04-29 12:02:50 +0200

    remove manual protoc install

--- scripts/install_protoc.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.11.4/protoc-3.11.4-linux-x86_64.zip -O protoc.zip
-unzip -o protoc.zip bin/protoc
-bin/protoc --version
