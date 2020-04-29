47238cc926164ef04f220afcaf59197f0db02ee7
Harry Urban <hurban@stroeer.de>
2020-04-29 16:49:28 +0200

    make node gen work

--- npmpublish.sh
+++ npmpublish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-ROOT=generated/main/ts
+ROOT=generated/main/node
+RELEASE_TAG=1.0.1
 cp package.json $ROOT
+cp .npmrc $ROOT
 cd $ROOT || exit 99
-npm pack
+npm pack --tag $RELEASE_TAG
