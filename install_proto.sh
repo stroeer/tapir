7a02b9d329fe92ee7bf957605f06786d82bca5b9
Harry Urban <hurban@stroeer.de>
2020-04-30 18:07:25 +0200

    fix protoc path

--- install_proto.sh
+++ install_proto.sh
@@ -4,7 +4,7 @@ wget $URL > protoc-gen-grpc-java
 chmod +x protoc-gen-grpc-java
 
 PROTOC_VERSION=3.11.4
-wget https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/protoc-$PROTOC_VERSION-linux-x86_64.zip -O protoc.zip
+wget https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/protoc-$PROTOC_VERSION-linux-x86_64.zip > protoc.zip
 unzip -o protoc.zip bin/protoc
-chmod +x protoc
-./protoc --version
+chmod +x bin/protoc
+./bin/protoc --version
