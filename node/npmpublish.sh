d8d1e43bd861bd7c020664a925f28b1ac71eaeba
Harry Urban <hurban@stroeer.de>
2020-04-30 13:34:03 +0200

    move node files to node folder

--- node/npmpublish.sh
+++ node/npmpublish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-ROOT=generated/main/node
-RELEASE_TAG=1.0.1
+ROOT=generated/main
 cp package.json $ROOT
+cp index.js $ROOT
 cp .npmrc $ROOT
 cd $ROOT || exit 99
 npm pack --tag $RELEASE_TAG
