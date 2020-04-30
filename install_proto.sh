71021ff2c1e120914d04bade23de0d2d707f4a24
Harry Urban <hurban@stroeer.de>
2020-04-30 17:28:27 +0200

    protoc on root level

--- install_proto.sh
+++ install_proto.sh
@@ -5,5 +5,5 @@ chmod +x protoc-gen-grpc-java
 
 PROTOC_VERSION=3.11.4
 wget https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/protoc-$PROTOC_VERSION-linux-x86_64.zip -O protoc.zip
-unzip -o protoc.zip bin/protoc
-bin/protoc --version
+unzip -o protoc.zip protoc
+./protoc --version
