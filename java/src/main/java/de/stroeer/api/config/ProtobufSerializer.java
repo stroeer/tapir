d4bae97fa758ca2f9086d6c1b788e254b71afb1c
Harry Urban <hurban@stroeer.de>
2020-05-11 17:25:20 +0200

    add Jackson Config Classes

---
+++ java/src/main/java/de/stroeer/api/config/ProtobufSerializer.java
@@ -0,0 +1,33 @@
+package de.stroeer.api.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.google.protobuf.GeneratedMessageV3;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import java.io.IOException;
+
+// borrowed from https://github.com/venth/training-webflux-grpc
+class ProtobufSerializer extends StdSerializer<GeneratedMessageV3> {
+
+  private final JsonFormat.Printer printer;
+
+  ProtobufSerializer() {
+    super(GeneratedMessageV3.class);
+    printer = JsonFormat.printer()
+        .preservingProtoFieldNames()
+        .omittingInsignificantWhitespace();
+  }
+
+  @Override
+  public Class<GeneratedMessageV3> handledType() {
+    return GeneratedMessageV3.class;
+  }
+
+  @Override
+  public void serialize(GeneratedMessageV3 value, JsonGenerator gen, SerializerProvider serializers)
+      throws IOException, InvalidProtocolBufferException {
+    gen.writeRawValue(printer.print(value));
+  }
+}
