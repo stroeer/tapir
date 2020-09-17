4bc28dcd1f85067f5fc84e8f940989e509545f1b
Patrick Dahms <pat.dahms@googlemail.com>
2020-09-17 12:08:20 +0200

    downcrade protoc back to 3.11.4

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,6 @@
 docker build -t stroeer/protoc-dockerized:latest \
 --build-arg NODE_GRPC_TOOLS_VERSION="1.9.0" \
---build-arg NODE_TS_PROTOC_GEN_VERSION="0.12.0" \
+--build-arg NODE_TS_PROTOC_GEN_VERSION="0.13.0" \
 --build-arg JAVA_LIB_VERSION="1.29.0" \
 --build-arg PROTOC_VERSION="3.13.0" \
 --build-arg GO_GRPC_TOOLS_VERSION="1.32.0" \
