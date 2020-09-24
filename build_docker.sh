479e74197ee32e1c81cdddccca1604a807a4cb56
Vitalij Krotov <vmk1vmk@users.noreply.github.com>
2020-09-24 12:30:07 +0200

    feature / GHCR (#48)
    * feature: replaced Docker Hub image URL with Github Container Registry (GHCR) image URL
    refactor: added newline to end of Shellscript to buil docker image
    
    * docs: updated URL to Docker image
    docs: updated requirements for docker image creation and release

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
-docker build -t stroeer/protoc-dockerized:latest \
+docker build -t ghcr.io/stroeer/protoc-dockerized:latest \
 --build-arg NODE_GRPC_TOOLS_VERSION="1.9.0" \
 --build-arg NODE_TS_PROTOC_GEN_VERSION="0.12.0" \
 --build-arg JAVA_LIB_VERSION="1.29.0" \
