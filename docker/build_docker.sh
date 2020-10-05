af2d042222b2572e313ea9729b3d65cd77cf48af
Vitalij Krotov <vmk1vmk@users.noreply.github.com>
2020-10-05 13:08:19 +0200

    feature: replaced explicit npm install commands with copying of the `package.json` and `yarn.lock` files and installing all dependencies from there refactor: removed version specification from docker build script `build_docker.sh`

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -8,9 +8,6 @@ protoc_version=3.13.0
 
 docker build \
 -t stroeer/protoc-dockerized:$protoc_version \
---build-arg NODE_GRPC_TOOLS_VERSION="1.9.1" \
---build-arg NODE_TS_PROTOC_GEN_VERSION="0.13.0" \
---build-arg JAVA_LIB_VERSION="1.32.1" \
 --build-arg PROTOC_VERSION=$protoc_version \
 --build-arg GO_GRPC_TOOLS_VERSION="1.32.0" \
 .
