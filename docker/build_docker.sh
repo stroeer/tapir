50f6f73ab394010b58d6f428f2905c82930c4102
Vitalij Krotov <vmk1vmk@users.noreply.github.com>
2020-10-05 13:21:25 +0200

    feature: added `go.mod` and `go.sum` files to pin go dependencies and their versions in an easily accessible location feature: tag docker image with GHCR path instead of plain name

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -7,7 +7,7 @@
 protoc_version=3.13.0
 
 docker build \
--t stroeer/protoc-dockerized:$protoc_version \
+-t ghcr.io/stroeer/protoc-dockerized:$protoc_version \
 --build-arg PROTOC_VERSION=$protoc_version \
 --build-arg GO_GRPC_TOOLS_VERSION="1.32.0" \
 .
