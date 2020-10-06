f6488704ef42aa8ec9edcb8582c826b80d4e151f
Vitalij Krotov <vmk1vmk@users.noreply.github.com>
2020-10-06 14:46:06 +0200

    refactor: removed go grpc version from build script

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -9,5 +9,4 @@ protoc_version=3.13.0
 docker build \
 -t ghcr.io/stroeer/protoc-dockerized:$protoc_version \
 --build-arg PROTOC_VERSION=$protoc_version \
---build-arg GO_GRPC_TOOLS_VERSION="1.32.0" \
 .
