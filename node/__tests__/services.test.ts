e4b2a2e79d695b2f9c7ce4631254376ad0ea350c
Moritz Zimmer <moritz.zmmr@gmail.com>
2020-12-01 10:00:22 +0100

    fixed node tests

--- node/__tests__/services.test.ts
+++ node/__tests__/services.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@jest/globals';
-import ArticleService from '../stroeer/pages/article/v1/article_service_grpc_pb';
-import SectionService from '../stroeer/pages/section/v1/section_service_grpc_pb';
+import ArticleService from '../stroeer/page/article/v1/article_page_service_grpc_pb';
+import SectionService from '../stroeer/page/section/v1/section_page_service_grpc_pb';
 
 test('gRPC article service client is generated and matches the snapshot', () => {
   expect(ArticleService).toMatchInlineSnapshot(`
@@ -8,7 +8,7 @@ test('gRPC article service client is generated and matches the snapshot', () => 
       "ArticlePageServiceClient": [Function],
       "ArticlePageServiceService": Object {
         "getArticlePage": Object {
-          "path": "/stroeer.pages.article.v1.ArticlePageService/GetArticlePage",
+          "path": "/stroeer.page.article.v1.ArticlePageService/GetArticlePage",
           "requestDeserialize": [Function],
           "requestSerialize": [Function],
           "requestStream": false,
@@ -29,7 +29,7 @@ test('gRPC section service client is generated and matches the snapshot', () => 
       "SectionPageServiceClient": [Function],
       "SectionPageServiceService": Object {
         "getSectionPage": Object {
-          "path": "/stroeer.pages.section.v1.SectionPageService/GetSectionPage",
+          "path": "/stroeer.page.section.v1.SectionPageService/GetSectionPage",
           "requestDeserialize": [Function],
           "requestSerialize": [Function],
           "requestStream": false,
