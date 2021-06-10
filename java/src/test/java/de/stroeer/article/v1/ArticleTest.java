dd668f40fa75fc548159639341044b2364b470c9
Matthias Naber <naber.matthias+github@gmail.com>
2021-06-10 10:17:28 +0200

    java sample

--- java/src/test/java/de/stroeer/article/v1/ArticleTest.java
+++ java/src/test/java/de/stroeer/article/v1/ArticleTest.java
@@ -1,17 +1,19 @@
 package de.stroeer.article.v1;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import de.stroeer.core.v1.Article;
 import de.stroeer.core.v1.Article.Builder;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ArticleTest {
 
   @Test
   void ModelCompiles() {
     Builder articleBuilder = Article.newBuilder();
-    articleBuilder.setId(42L);
+    articleBuilder.setId(42L)
+      // such nice enums. I luv it.
+      .setType(Article.ContentType.ARTICLE);
 
     Article article = articleBuilder.build();
 
