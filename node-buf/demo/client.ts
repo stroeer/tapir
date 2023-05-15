69993f0ef776add5a887d1aede5f888478575d7c
Patrick Dahms <pat.dahms@googlemail.com>
2023-05-15 13:51:24 +0200

    feat(node-buf): init

---
+++ node-buf/demo/client.ts
@@ -0,0 +1,48 @@
+import fetch from 'node-fetch';
+import { GetNavigationResponse } from '../stroeer/navigation/v1/navigation_service_pb';
+
+const validateEnvVar = (env: string | undefined, name: string): string => {
+  if (!env || env.length < 1) {
+    throw new Error(`You need to provide an env var "${name}"`);
+  }
+
+  return env;
+};
+
+const getAPIEndpoint = () => {
+  const apiEndpoint = process.env['API_ENDPOINT'];
+
+  return validateEnvVar(apiEndpoint, 'API_ENDPOINT');
+};
+
+const getAuthorizationHeader = () => {
+  const header = process.env['AUTH_HEADER'];
+
+  return validateEnvVar(header, 'AUTH_HEADER');
+};
+
+function fetchData() {
+  return fetch(getAPIEndpoint(), {
+    headers: {
+      'user-agent': 'tapir_node_buf_demo',
+      Authorization: getAuthorizationHeader(),
+    },
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(
+        `The response is not ok. Status: ${response.status}. Text: ${response.statusText}`
+      );
+    }
+    return response.json();
+  });
+}
+
+fetchData()
+  .then((data) => {
+    console.log('success');
+    const navigationResponse = GetNavigationResponse.fromJson(data);
+    console.log({ navi: navigationResponse.navigationMenu });
+  })
+  .catch((error) => {
+    console.error('There was an error while fetching the data', error);
+  });
